3ac4f594fd52f329e9b0b495e0da7cc3e9b35d95
Алиса Кондратенко <78209371+Alice-pinguin@users.noreply.github.com>
2021-04-26 17:54:01 +0300

    Update IQueue.java

--- src/Collection/IQueue.java
+++ src/Collection/IQueue.java
@@ -1,10 +1,10 @@
 package Collection;
 
 public interface IQueue <T> {
-    void add(Object value);
-    Object remove(int index);
+    void add(T value);
+    T remove(int index);
     void clear();
     int size();
     T peek();
-    int pop();
+    T poll();
 }
